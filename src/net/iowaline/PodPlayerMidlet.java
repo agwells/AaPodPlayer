c83cfab8eb086b8a81fc7e30414155884fbc5681
unknown <aaron@.(none)>
2010-06-07 23:28:17 +1200

    Issue5: It now remembers the last file and returns to it

--- src/net/iowaline/PodPlayerMidlet.java
+++ src/net/iowaline/PodPlayerMidlet.java
@@ -14,12 +14,16 @@ import javax.microedition.lcdui.*;
 import javax.microedition.media.Manager;
 import javax.microedition.media.Player;
 import javax.microedition.media.control.VolumeControl;
+import javax.microedition.rms.RecordStore;
+import javax.microedition.rms.RecordStoreException;
 import org.netbeans.microedition.lcdui.pda.FileBrowser;
 
 /**
  * @author aaron
  */
 public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStateListener {
+    private static final String LAST_FILE_RECORDSTORE = "lastFilePlaying";
+    private static final String LAST_PLAYTIME_RECORDSTORE = "lastPlayTime";
 
     private boolean midletPaused = false;
 
@@ -28,8 +32,9 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
     private Form screenPlayer;
     private Gauge volumeGauge;
     private Gauge timeGauge;
+    private Command cmdChangeTrack;
     private Command cmdBack15Sec;
-    private Command cmdQuit1;
+    private Command cmdFileBrowserQuit;
     private Command cmdPlayerQuit;
     private Command cmdPlayerPause;
     private Ticker ticker;
@@ -39,6 +44,7 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
     private VolumeControl volumeControl;
     private Timer timeDisplayTimer;
     private UpdateDisplayTask timeTask;
+    private String currentFileUrl;
 
     /**
      * 
@@ -90,17 +96,15 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
 
     public class UpdateDisplayTask extends TimerTask{
 
-        private Screen playerScreen;
         private Player mp3Player;
         private Gauge timeGauge;
         private long time = 0;
         private long mediaDuration = 0;
         private int timeGaugeMaxValue;
         
-        public UpdateDisplayTask( Player mp3Player, Gauge timeGauge, Screen playerScreen ){
+        public UpdateDisplayTask( Player mp3Player, Gauge timeGauge ){
             this.mp3Player = mp3Player;
             this.timeGauge = timeGauge;
-            this.playerScreen = playerScreen;
             this.mediaDuration = mp3Player.getDuration();
             this.timeGaugeMaxValue = timeGauge.getMaxValue();
         }
@@ -169,9 +173,23 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
         screenPlayer.addCommand(getCmdPlayerPause());
         screenPlayer.addCommand(getCmdPlayerQuit());
         screenPlayer.addCommand(getCmdBack15Sec());
+        screenPlayer.addCommand(getCmdChangeTrack());
         screenPlayer.setCommandListener(this);//GEN-END:|0-initialize|1|0-postInitialize
         // write post-initialize user code here
         screenPlayer.setItemStateListener(this);
+        currentFileUrl = null;
+        try {
+            RecordStore store = RecordStore.openRecordStore(LAST_FILE_RECORDSTORE, true);
+            int numRecords = store.getNumRecords();
+            System.out.println("numRecords: " + Integer.toString(numRecords));
+            if ( store.getNumRecords() > 0 ){
+                currentFileUrl = new String(store.getRecord(1));
+            }
+            store.closeRecordStore();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }//GEN-BEGIN:|0-initialize|2|
     //</editor-fold>//GEN-END:|0-initialize|2|
 
@@ -182,7 +200,7 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
     public void startMIDlet() {//GEN-END:|3-startMIDlet|0|3-preAction
         // write pre-action user code here
         //System.out.println("Heyo");
-        switchDisplayable(null, getScreenFileBrowser());//GEN-LINE:|3-startMIDlet|1|3-postAction
+        isLastFileNameSaved();//GEN-LINE:|3-startMIDlet|1|3-postAction
         // write post-action user code here
     }//GEN-BEGIN:|3-startMIDlet|2|
     //</editor-fold>//GEN-END:|3-startMIDlet|2|
@@ -227,13 +245,14 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
         if (displayable == screenFileBrowser) {//GEN-BEGIN:|7-commandAction|1|16-preAction
             if (command == FileBrowser.SELECT_FILE_COMMAND) {//GEN-END:|7-commandAction|1|16-preAction
 
-                String mp3FileUrl = screenFileBrowser.getSelectedFileURL();
+                currentFileUrl = screenFileBrowser.getSelectedFileURL();
                 try {
-                    mp3Player = Manager.createPlayer(mp3FileUrl);
+                    mp3Player = Manager.createPlayer(currentFileUrl);
                     mp3Player.realize();
                     mp3Player.prefetch();
                     volumeControl = (VolumeControl) mp3Player.getControl("VolumeControl");
-                    timeTask = new UpdateDisplayTask(mp3Player, timeGauge, screenPlayer);
+                    timeTask = new UpdateDisplayTask(mp3Player, timeGauge);
+
                     timeDisplayTimer.scheduleAtFixedRate(timeTask, 0, 500);
                 } catch (IOException ex) {
                     ex.printStackTrace();
@@ -241,11 +260,11 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
                     ex.printStackTrace();
                 }
 
-                ticker.setString(mp3FileUrl);
+                ticker.setString(currentFileUrl);
 
                 switchDisplayable(null, screenPlayer);//GEN-LINE:|7-commandAction|2|16-postAction
                 // write post-action user code here
-            } else if (command == cmdQuit1) {//GEN-LINE:|7-commandAction|3|19-preAction
+            } else if (command == cmdFileBrowserQuit) {//GEN-LINE:|7-commandAction|3|19-preAction
                 // write pre-action user code here
                 exitMIDlet();//GEN-LINE:|7-commandAction|4|19-postAction
                 // write post-action user code here
@@ -256,7 +275,21 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
                 adjustPlayerTimeBySeconds(-15);
 //GEN-LINE:|7-commandAction|6|67-postAction
                 // write post-action user code here
-            } else if (command == cmdPlayerPause) {//GEN-LINE:|7-commandAction|7|62-preAction
+            } else if (command == cmdChangeTrack) {//GEN-LINE:|7-commandAction|7|69-preAction
+                // write pre-action user code here
+                switchDisplayable(null, getScreenFileBrowser());//GEN-LINE:|7-commandAction|8|69-postAction
+                // write post-action user code here
+                mp3Player.close();
+                mp3Player = null;
+                timeTask.cancel();
+                timeTask = null;
+                try {
+                    // Clear the RecordStore if it exists, before saving this one
+                    RecordStore.deleteRecordStore(LAST_FILE_RECORDSTORE);
+                } catch (RecordStoreException ex) {
+                    ex.printStackTrace();
+                }
+            } else if (command == cmdPlayerPause) {//GEN-LINE:|7-commandAction|9|62-preAction
                 // write pre-action user code here
                 try {
                     if ( mp3Player.getState() == Player.STARTED ){
@@ -267,17 +300,30 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
                 } catch (MediaException ex){
                     ex.printStackTrace();
                 }
-//GEN-LINE:|7-commandAction|8|62-postAction
+//GEN-LINE:|7-commandAction|10|62-postAction
                 // write post-action user code here
-            } else if (command == cmdPlayerQuit) {//GEN-LINE:|7-commandAction|9|64-preAction
-//                // write pre-action user code here
-                exitMIDlet();//GEN-LINE:|7-commandAction|10|64-postAction
+            } else if (command == cmdPlayerQuit) {//GEN-LINE:|7-commandAction|11|64-preAction
+                try {
+                    // Clear the RecordStore if it exists, before saving this one
+                    RecordStore.deleteRecordStore(LAST_FILE_RECORDSTORE);
+                } catch (RecordStoreException ex) {
+                    ex.printStackTrace();
+                }
+
+                try {
+                    RecordStore store = RecordStore.openRecordStore(LAST_FILE_RECORDSTORE, true);
+                    store.addRecord(currentFileUrl.getBytes(), 0, currentFileUrl.getBytes().length);
+                    store.closeRecordStore();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                exitMIDlet();//GEN-LINE:|7-commandAction|12|64-postAction
                 // write post-action user code here
-            }//GEN-BEGIN:|7-commandAction|11|7-postCommandAction
-        }//GEN-END:|7-commandAction|11|7-postCommandAction
+            }//GEN-BEGIN:|7-commandAction|13|7-postCommandAction
+        }//GEN-END:|7-commandAction|13|7-postCommandAction
         // write post-action user code here
-    }//GEN-BEGIN:|7-commandAction|12|
-    //</editor-fold>//GEN-END:|7-commandAction|12|
+    }//GEN-BEGIN:|7-commandAction|14|
+    //</editor-fold>//GEN-END:|7-commandAction|14|
 
     //<editor-fold defaultstate="collapsed" desc=" Generated Getter: screenFileBrowser ">//GEN-BEGIN:|14-getter|0|14-preInit
     /**
@@ -292,25 +338,26 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
             screenFileBrowser.setCommandListener(this);
             screenFileBrowser.setFilter("");
             screenFileBrowser.addCommand(FileBrowser.SELECT_FILE_COMMAND);
-            screenFileBrowser.addCommand(getCmdQuit1());//GEN-END:|14-getter|1|14-postInit
+            screenFileBrowser.addCommand(getCmdFileBrowserQuit());//GEN-END:|14-getter|1|14-postInit
             // write post-init user code here
         }//GEN-BEGIN:|14-getter|2|
         return screenFileBrowser;
     }
     //</editor-fold>//GEN-END:|14-getter|2|
+    //</editor-fold>
 
-    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: cmdQuit1 ">//GEN-BEGIN:|18-getter|0|18-preInit
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: cmdFileBrowserQuit ">//GEN-BEGIN:|18-getter|0|18-preInit
     /**
-     * Returns an initiliazed instance of cmdQuit1 component.
+     * Returns an initiliazed instance of cmdFileBrowserQuit component.
      * @return the initialized component instance
      */
-    public Command getCmdQuit1() {
-        if (cmdQuit1 == null) {//GEN-END:|18-getter|0|18-preInit
+    public Command getCmdFileBrowserQuit() {
+        if (cmdFileBrowserQuit == null) {//GEN-END:|18-getter|0|18-preInit
             // write pre-init user code here
-            cmdQuit1 = new Command("Exit", Command.EXIT, 0);//GEN-LINE:|18-getter|1|18-postInit
+            cmdFileBrowserQuit = new Command("Exit", Command.EXIT, 0);//GEN-LINE:|18-getter|1|18-postInit
             // write post-init user code here
         }//GEN-BEGIN:|18-getter|2|
-        return cmdQuit1;
+        return cmdFileBrowserQuit;
     }
     //</editor-fold>//GEN-END:|18-getter|2|
 
@@ -368,7 +415,7 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
     public Command getCmdPlayerPause() {
         if (cmdPlayerPause == null) {//GEN-END:|61-getter|0|61-preInit
             // write pre-init user code here
-            cmdPlayerPause = new Command("Play/Pause", Command.OK, 0);//GEN-LINE:|61-getter|1|61-postInit
+            cmdPlayerPause = new Command("Play/Pause", Command.OK, 1);//GEN-LINE:|61-getter|1|61-postInit
             // write post-init user code here
         }//GEN-BEGIN:|61-getter|2|
         return cmdPlayerPause;
@@ -383,7 +430,7 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
     public Command getCmdPlayerQuit() {
         if (cmdPlayerQuit == null) {//GEN-END:|63-getter|0|63-preInit
             // write pre-init user code here
-            cmdPlayerQuit = new Command("Quit", Command.EXIT, 0);//GEN-LINE:|63-getter|1|63-postInit
+            cmdPlayerQuit = new Command("Quit", Command.EXIT, 15);//GEN-LINE:|63-getter|1|63-postInit
             // write post-init user code here
         }//GEN-BEGIN:|63-getter|2|
         return cmdPlayerQuit;
@@ -399,12 +446,61 @@ public class PodPlayerMidlet extends MIDlet implements CommandListener, ItemStat
     public Command getCmdBack15Sec() {
         if (cmdBack15Sec == null) {//GEN-END:|66-getter|0|66-preInit
             // write pre-init user code here
-            cmdBack15Sec = new Command("15s", Command.ITEM, 0);//GEN-LINE:|66-getter|1|66-postInit
+            cmdBack15Sec = new Command("15s", Command.ITEM, 2);//GEN-LINE:|66-getter|1|66-postInit
             // write post-init user code here
         }//GEN-BEGIN:|66-getter|2|
         return cmdBack15Sec;
     }
     //</editor-fold>//GEN-END:|66-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: cmdChangeTrack ">//GEN-BEGIN:|68-getter|0|68-preInit
+    /**
+     * Returns an initiliazed instance of cmdChangeTrack component.
+     * @return the initialized component instance
+     */
+    public Command getCmdChangeTrack() {
+        if (cmdChangeTrack == null) {//GEN-END:|68-getter|0|68-preInit
+            // write pre-init user code here
+            cmdChangeTrack = new Command("Change Track", Command.BACK, 15);//GEN-LINE:|68-getter|1|68-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|68-getter|2|
+        return cmdChangeTrack;
+    }
+    //</editor-fold>//GEN-END:|68-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: isLastFileNameSaved ">//GEN-BEGIN:|71-if|0|71-preIf
+    /**
+     * Performs an action assigned to the isLastFileNameSaved if-point.
+     */
+    public void isLastFileNameSaved() {//GEN-END:|71-if|0|71-preIf
+        // enter pre-if user code here
+        if (currentFileUrl == null) {//GEN-LINE:|71-if|1|72-preAction
+            // write pre-action user code here
+            switchDisplayable(null, getScreenFileBrowser());//GEN-LINE:|71-if|2|72-postAction
+            // write post-action user code here
+        } else {//GEN-LINE:|71-if|3|73-preAction
+                try {
+                    mp3Player = Manager.createPlayer(currentFileUrl);
+                    mp3Player.realize();
+                    mp3Player.prefetch();
+                    volumeControl = (VolumeControl) mp3Player.getControl("VolumeControl");
+                    timeTask = new UpdateDisplayTask(mp3Player, timeGauge);
+
+                    timeDisplayTimer.scheduleAtFixedRate(timeTask, 0, 500);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (MediaException ex) {
+                    ex.printStackTrace();
+                }
+
+                ticker.setString(currentFileUrl);
+
+                switchDisplayable(null, screenPlayer);//GEN-LINE:|71-if|4|73-postAction
+            // write post-action user code here
+        }//GEN-LINE:|71-if|5|71-postIf
+        // enter post-if user code here
+    }//GEN-BEGIN:|71-if|6|
+    //</editor-fold>//GEN-END:|71-if|6|
 
     /**
      * Returns a display instance.
